ab46217ec9bf79b637c26a87a69e7f22f0fdf4f9
angel <angel.velasco0038@gmail.com>
2025-04-06 20:06:14 +0200

    Projet ajout

---
+++ src/Dao/ProduitDAO.java
@@ -0,0 +1,35 @@
+package Dao;
+
+import Modele.Produit;
+import java.sql.*;
+import java.util.*;
+
+public class ProduitDAO {
+
+    public List<Produit> getAllProduits() {
+        List<Produit> produits = new ArrayList<>();
+        String sql = "SELECT * FROM produit";
+
+        try (Connection con = JdbcDataSource.getConnection();
+             PreparedStatement stmt = con.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                produits.add(new Produit(
+                        rs.getInt("id"),
+                        rs.getString("nom"),
+                        rs.getString("description"),
+                        rs.getInt("quantite"),
+                        rs.getDouble("prix"),
+                        rs.getString("image"),
+                        rs.getString("category"),
+                        rs.getInt("marque_id")
+                ));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return produits;
+    }
+}
--- src/Vue/VentesFlashPage.java
+++ src/Vue/VentesFlashPage.java
@@ -1,9 +1,49 @@
 package Vue;
 
+import Dao.ProduitDAO;
+import Modele.Produit;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.List;
 
 public class VentesFlashPage extends JPanel {
     public VentesFlashPage() {
-        add(new JLabel("⚡ Offres en vente flash !"));
+        setLayout(new BorderLayout());
+
+        // récupérer les produits depuis la base de données
+        ProduitDAO produitDAO = new ProduitDAO();
+        List<Produit> produits = produitDAO.getAllProduits();
+
+        // Définition des colonnes pour le tableau
+        String[] columnNames = {"Nom", "Description", "Quantité", "Prix", "Catégorie", "Image"};
+
+        // modèle du tableau avec les produits
+        DefaultTableModel model = new DefaultTableModel(columnNames, 0);
+        JTable table = new JTable(model);
+
+        // remplir le modèle avec les produits récupérés
+        for (Produit produit : produits) {
+            model.addRow(new Object[]{
+                    produit.getNomProduit(),
+                    produit.getDescription(),
+                    produit.getQuantite(),
+                    produit.getPrix() + "€",
+                    produit.getCategorie(),
+                    produit.getImage()
+            });
+        }
+
+        //  le tableau et pour scroll
+        JScrollPane scrollPane = new JScrollPane(table);
+        table.setFillsViewportHeight(true);
+        add(scrollPane, BorderLayout.CENTER);
+
+        // Titre
+        JLabel title = new JLabel("Produits en Vente", SwingConstants.CENTER);
+        title.setFont(new Font("Arial", Font.BOLD, 24));
+        add(title, BorderLayout.NORTH);
     }
-}
+    }
+
